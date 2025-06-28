646af014fffe81602c6fbd691be419799b9e6475
Anshul Dubey <dubeyanshul2204@gmail.com>
2025-06-28 14:53:26 +0530

    commit

---
+++ Day-2/practice.js
@@ -0,0 +1,33 @@
+//!-- 1
+
+let num = 1;
+
+if (num > 0) {
+  console.log("Positive");
+} else if (num < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
+}
+
+//!--2
+
+let num1 = 12;
+
+if (num1 % 2 == 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+
+//!--3
+
+let age = 19;
+
+if (age >= 18) {
+  console.log("You are an adult");
+} else if (age >= 13 && age <= 20{
+  console.log("You are a teenager");
+} else f
+  console.log("You are not an adult");
+}
