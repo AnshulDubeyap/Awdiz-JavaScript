83bd5b87f9eba10dad40ea0c131bf82e18793514
Anshul Dubey <dubeyanshul2204@gmail.com>
2025-06-28 08:55:41 +0530

    Day-1 Javascript

---
+++ conditionals.js
@@ -0,0 +1,39 @@
+//! Conditionals in Javascript
+
+let age = 18;
+
+if (age >= 18) {
+  console.log("You are an adult");
+} else {
+  console.log("You are not an adult");
+}
+
+//! Switch in JavaScript
+
+let day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("Today is Monday");
+    break;
+  case "tuesday":
+    console.log("Today is Tuesday");
+    break;
+  case "wednesday":
+    console.log("Today is Wednesday");
+    break;
+  case "thursday":
+    console.log("Today is Thursday");
+    break;
+  case "friday":
+    console.log("Today is Friday");
+    break;
+  case "saturday":
+    console.log("Today is Saturday");
+    break;
+  case "sunday":
+    console.log("Today is Sunday");
+    break;
+  default:
+    console.log("Not a valid day");
+}
